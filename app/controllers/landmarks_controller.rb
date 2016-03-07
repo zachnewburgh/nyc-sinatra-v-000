20e16699c6f0c05984d6ac950b4e805445cff43a
zachnewburgh <zach.newburgh@gmail.com>
2016-03-07 12:26:44 -0500

    nyc guide for landmarks, figures, titles

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,33 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params["landmark"])
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params["id"])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params["id"])
+    erb :'landmarks/edit'
+  end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params["id"])
+    @landmark.update(params["landmark"])
+    erb :'landmarks/show'
+  end
+
 end
--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,3 +1,3 @@
-class Landmark <ActiveRecord::Base
-
+class Landmark < ActiveRecord::Base
+  belongs_to :figure
 end
